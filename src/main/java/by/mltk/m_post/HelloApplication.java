aeaf905dd6f98b3057d1bde38ae94c6414e943f0
Heorhi Puhachou <ce-heorhi.puhachou@beyondnow.com>
2023-06-23 16:21:02 +0200

    Update layout.

--- src/main/java/by/mltk/m_post/HelloApplication.java
+++ src/main/java/by/mltk/m_post/HelloApplication.java
@@ -5,62 +5,80 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import static javafx.geometry.Pos.CENTER;
 
 public class HelloApplication extends Application {
 
     private static final TaraskLacinkConverter converter = new TaraskLacinkConverter();
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Welcome");
+        int row = 0;
+        int column = 0;
+
+        primaryStage.setTitle("Malatok2334");
 
         GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
+        grid.setAlignment(CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-       // Text scenetitle = new Text("Aryhinalny tekst");
+        //Original text
         TextArea originalTextArea = new TextArea();
-       // scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(originalTextArea, 0, 0, 2, 1);
+        grid.add(originalTextArea, column, row, 2, 1);
 
-
-        Label userName = new Label("Telegram:");
-        grid.add(userName, 0, 1);
-        TextArea inputPost = new TextArea();
-        grid.add(inputPost, 0, 2);
-        TextArea outputPost = new TextArea();
-
-
-        grid.add(outputPost, 1, 2);
-
-
-        Button btn = new Button("Sign in");
+        Button btn = new Button("Padrychtavac");
         HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.setAlignment(CENTER);
         hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
 
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
+        grid.add(hbBtn, 0, ++row, 2, 1);
+
+
+        Label mastadonLabel = new Label("Mastadon:");
+        grid.add(mastadonLabel, 0, ++row);
+        TextArea mastadonPost = new TextArea();
+        grid.add(mastadonPost, 0, ++row);
+
+        row--;
+        row--;
+
+        Label telegramLabel = new Label("Telegram:");
+        grid.add(telegramLabel, 1, ++row);
+        TextArea telegramPost = new TextArea();
+        grid.add(telegramPost, 1, ++row);
+
+        Label twitterLabel = new Label("Twitter:");
+        grid.add(twitterLabel, 0, ++row);
+        TextArea twitterPost = new TextArea();
+        grid.add(twitterPost, 0, ++row);
+
+        row--;
+        row--;
+
+        Label facebookLabel = new Label("Facebook:");
+        grid.add(facebookLabel, 1, ++row);
+        TextArea facebookPost = new TextArea();
+        grid.add(facebookPost, 1, ++row);
+
+
+
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent e) {
-                outputPost.setText(converter.convert(inputPost.getText()));
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
+                mastadonPost.setText(converter.convert(originalTextArea.getText()));
+                telegramPost.setText(originalTextArea.getText() + "\n\n" + converter.convert(originalTextArea.getText()));
             }
         });
 
@@ -69,15 +87,6 @@ public class HelloApplication extends Application {
 
         primaryStage.show();
     }
-
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
 
     public static void main(String[] args) {
         launch();
